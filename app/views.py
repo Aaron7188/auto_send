c276edd08d2e90825064c69c74f16ad90c51d440
hcf <huangchenfeng718@gmail.com>
2024-08-07 14:32:52 +0800

    选择时优化下拉框

--- app/views.py
+++ app/views.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template
 from app import db, scheduler
 from app.models import User, Channel, Tweet
 from app.utils import insert_users_to_db, insert_channels_to_db, send_message_to_channel
 import datetime
 
 bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
 
 @bp.route('/upload/users', methods=['POST'])
 def upload_users():
@@ -28,13 +32,27 @@ def upload_channels():
 
 @bp.route('/send-tweets', methods=['POST'])
 def send_tweets():
-    country = request.form['country']
-    category = request.form['category']
+    country = request.form.get('country')
+    category = request.form.get('category')
     count = int(request.form['count'])
     interval = int(request.form['interval'])
 
-    tweets = Tweet.query.filter_by(country=country, category=category, status='Pending').limit(count).all()
-    channels = Channel.query.filter_by(country=country, category=category).all()
+    query = Tweet.query.filter_by(status='Pending')
+
+    if country:
+        query = query.filter_by(country=country)
+    if category:
+        query = query.filter_by(category=category)
+
+    tweets = query.limit(count).all()
+    channels_query = Channel.query
+
+    if country:
+        channels_query = channels_query.filter_by(country=country)
+    if category:
+        channels_query = channels_query.filter_by(category=category)
+
+    channels = channels_query.all()
 
     for i, tweet in enumerate(tweets):
         scheduler.add_job(send_message_to_channel, 'date', run_date=datetime.datetime.now() + datetime.timedelta(milliseconds=interval * i), args=[tweet, channels])
